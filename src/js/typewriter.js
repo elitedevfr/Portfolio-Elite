631a4eababfdba0ddeb337c820102782742b90c4
kiir0n <88636812+kiir0n@users.noreply.github.com>
2023-06-19 11:31:13 +0900

    Create typewriter.js

---
+++ src/js/typewriter.js
@@ -0,0 +1,41 @@
+  
+  const texts = ['Usually i just write boring code that probably doesn\'t even work', 'when project becomes booring i just discard it', 'Lastly i work in teams such as Kaskade'];
+  let currentTextIndex = 0;
+  let currentCharIndex = 0;
+  let isDeleting = false;
+  let typingDelay = 100;
+  let erasingDelay = 50;
+  let newTextDelay = 2000;
+
+  const typewriter = document.getElementById('typewriter');
+
+  function type() {
+    const currentText = texts[currentTextIndex];
+
+    if (isDeleting) {
+      typewriter.textContent = currentText.substring(0, currentCharIndex - 1);
+      currentCharIndex--;
+      typingDelay = erasingDelay;
+    } else {
+      typewriter.textContent = currentText.substring(0, currentCharIndex + 1);
+      currentCharIndex++;
+      typingDelay = 100;
+    }
+
+    if (!isDeleting && currentCharIndex === currentText.length) {
+      isDeleting = true;
+      typingDelay = newTextDelay;
+    } else if (isDeleting && currentCharIndex === 0) {
+      isDeleting = false;
+      currentTextIndex++;
+      typingDelay = 500;
+      if (currentTextIndex === texts.length) {
+        currentTextIndex = 0;
+      }
+    }
+
+    setTimeout(type, typingDelay);
+  }
+
+  type();
+  
